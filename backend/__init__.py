201a9d54dc09acd3540b3f8d50d59fb6ead509d9
hujuu <out.of.the.base.sfc@gmail.com>
2022-08-02 10:48:42 +0900

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
@@ -17,15 +16,6 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 app.config.from_object(Config)
 migrate = Migrate(app, db) #https://qiita.com/svfreerider/items/50c68252c2f119583a28
 db.init_app(app)
-
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.init_app(app)
-
-@login_manager.user_loader
-def load_user(user_id):
-    # since the user_id is just the primary key of our user table, use it in the query for the user
-    return User.query.get(int(user_id))
 
 # blueprint for auth routes in our app
 from .auth import auth as auth_blueprint

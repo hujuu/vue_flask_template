6b42888ba5d98f9df828eb715509e500aea9db5a
hujuu <out.of.the.base.sfc@gmail.com>
2022-08-02 10:47:49 +0900

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config(object):
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") or "supersekrit"
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "sqlite:///app.sqlite3"
